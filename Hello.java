2d1d9616725acbbb62876af8e44d5aebc7d94b7d
rakuna2017 <46988399+rakuna2017@users.noreply.github.com>
2019-01-25 16:02:22 +0900

    SUN値ピンチ！
    (」・ω・)」うー！(／・ω・)／にゃー！

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+	public static void main(String[] hoge) {
+		System.out.println("Hello!(>w<) Hell?(・ω<)ノ");
+		System.out.println("SUN値ピンチ！ SUN値ピンチ！SUN値ピンチ！");
+		System.out.println("SUN値ピンチ！ SUN値ピンチ！SUN値ピンチ！");
+	}
+}
